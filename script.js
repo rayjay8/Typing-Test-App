8e5c4b3446a7a7d11fbee07486f63c810547d80c
rayjay8 <76774054+rayjay8@users.noreply.github.com>
2022-12-25 22:50:54 +0530

    Fixed minor issues and added cursor functionality

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const content = document.getElementById("content");
 const btn = document.getElementById("btn");
 const popup = document.getElementById("popup");
 const wpm = document.getElementById("wpm");
+const title = document.getElementById("title");
 const time = 1;
 
 let typed = 0;
@@ -14,21 +15,21 @@ const splitText = text.split("");
 const words = text.split(" ");
 const wordCount = words.length;
 
-// const cursor = document.createElement("span");
-// cursor.setAttribute("id", "iCursor");
-// cursor.textContent = "|";
-// box.appendChild(cursor);
-// var c = true;
-// var speed = 500;
-// setInterval(function () {
-//   if (c) {
-//     document.getElementById("iCursor").style.opacity = 0;
-//     c = false;
-//   } else {
-//     document.getElementById("iCursor").style.opacity = 1;
-//     c = true;
-//   }
-// }, speed);
+const cursor = document.createElement("span");
+cursor.setAttribute("id", "iCursor");
+cursor.textContent = "|";
+box.appendChild(cursor);
+var c = true;
+var speed = 500;
+setInterval(function () {
+  if (c) {
+    document.getElementById("iCursor").style.opacity = 0;
+    c = false;
+  } else {
+    document.getElementById("iCursor").style.opacity = 1;
+    c = true;
+  }
+}, speed);
 
 let i = 0;
 
@@ -43,14 +44,15 @@ function test() {
   let j = 0;
 
   document.addEventListener("keypress", (e) => {
+    const char = document.getElementById("letter" + j);
     if (e.key === splitText[j]) {
-      const char = document.getElementById("letter" + j);
       char.style.color = "green";
+      char.insertAdjacentElement("afterend", cursor);
       typed++;
     } else {
-      const char = document.getElementById("letter" + j);
       char.textContent = e.key;
       char.style.color = "red";
+      char.insertAdjacentElement("afterend", cursor);
     }
     j++;
   });
@@ -59,6 +61,7 @@ function test() {
     if (e.key === "Backspace") {
       j--;
       const char = document.getElementById("letter" + j);
+      char.insertAdjacentElement("beforebegin", cursor);
       char.style.color = "black";
       if (e.key !== splitText[j]) {
         char.textContent = splitText[j];
@@ -69,6 +72,7 @@ function test() {
   document.body.onkeyup = function (e) {
     if (e.keyCode == 32) {
       const char = document.getElementById("letter" + j);
+      char.insertAdjacentElement("afterend", cursor);
       char.style.color = "black";
       if (e.key !== splitText[j]) {
         char.textContent = splitText[j];
@@ -79,11 +83,12 @@ function test() {
   };
 }
 
-btn.addEventListener("click", () => {
-  test();
+function start() {
   let i = 59;
   let timer = setInterval(() => {
     btn.style.backgroundColor = "blue";
+    btn.style.width = "134px";
+    btn.style.padding = "15px 40px";
     if (i < 10) {
       btn.innerHTML = "00:0" + i--;
     } else {
@@ -92,22 +97,38 @@ btn.addEventListener("click", () => {
     if (i > 0 && i < 10) {
       btn.style.backgroundColor = "red";
     }
+    if (btn.innerHTML === "00:01") {
+      btn.style.backgroundColor = "red";
+    }
     if (i == 0) {
-      clearInterval(timer);
-      btn.innerHTML = "Restart";
-      btn.style.backgroundColor = "green";
-      popup.style.display = "flex";
-      wpm.innerHTML = Math.round(typed / 5 / time) + " WPM";
-      btn.addEventListener("click", () => {
-        location.reload();
+      let wait = setInterval(() => {
+        clearInterval(timer);
+        btn.innerHTML = "";
+        btn.style.backgroundColor = "green";
+        btn.style.width = "54px";
+        btn.style.padding = "0px";
+        let stopIcon = document.createElement("i");
+        stopIcon.setAttribute("class", "fas fa-stop");
+        btn.appendChild(stopIcon);
+        wpm.innerHTML = Math.round(typed / 5 / time) + " WPM";
+      }, 1000);
+      let popupTimeout = setTimeout(() => {
+        popup.style.display = "flex";
+      }, 1000);
+      document.addEventListener("mouseup", function (e) {
+        if (!popup.contains(e.target)) {
+          popup.style.display = "none";
+          location.reload();
+        }
       });
     }
   }, 1000);
-});
+}
 
-document.addEventListener("mouseup", function (e) {
-  if (!popup.contains(e.target)) {
-    popup.style.display = "none";
-    // location.reload();
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    start();
+    test();
   }
 });
